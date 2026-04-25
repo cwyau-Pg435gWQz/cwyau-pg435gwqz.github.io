a9b0be723f429a8de7f4a966144d2e89ff4485f9
Raymond Kwok <rmwkwok@gmail.com>
2026-04-25 14:30:20 +0800

    update all links with all schoolbag inputs

--- assets/js/custom/schoolbag_checklist.js
+++ assets/js/custom/schoolbag_checklist.js
@@ -1,6 +1,9 @@
-const checkedItemsArea = document.getElementById('checked-items-area');
 const copyBtn = document.getElementById('checked-items-copy-btn');
+const checkedItemsArea = document.getElementById('checked-items-area');
 const checkedItemsText = document.getElementById('checked-items-text');
+const weightResultArea = document.getElementById('weight-result');
+const checkboxResultArea = document.getElementById('checklist-result');
+
 const googleFormAnchor = document.getElementById('google-form');
 const googleFormURL = googleFormAnchor.href;
 const whatsappFormAnchor = document.getElementById('whatsapp-form');
@@ -42,8 +45,15 @@ function validateNumericInput(input) {
   input.value = value;
 }
 
-function updateChecklistStatus() {
-  // For displaying checked items
+function updateSchoolBagFormStatus() {
+  const kidWeight = parseFloat(document.getElementById('checklist-item10-1').value);
+  const bagWeight = parseFloat(document.getElementById('checklist-item10-2').value);
+
+  const checkboxGroup1Count = document.querySelectorAll('input.checklist-group1:checked').length;
+  const checkboxGroup2Count = document.querySelectorAll('input.checklist-group2:checked').length;
+  const checkboxGroup3Count = document.querySelectorAll('input.checklist-group3:checked').length;
+  const checkboxGroup4Count = document.querySelectorAll('input.checklist-group4:checked').length;
+
   const checkedBoxes = document.querySelectorAll('input.checklist:checked');
   const checkedTexts = Array.from(checkedBoxes)
                             .map(cb => {
@@ -51,61 +61,53 @@ function updateChecklistStatus() {
                               return label ? label.textContent.trim() : "";
                             });
 
+  let whatsappText = "";
+  let googleFormQuery = [];
+  if (!Number.isNaN(kidWeight)) {
+    googleFormQuery.push("entry.2145623731=" + kidWeight);
+    whatsappText += `體重: ${kidWeight}kg\n`;
+  }
+  if (!Number.isNaN(bagWeight)) {
+    googleFormQuery.push("entry.623543962=" + bagWeight);
+    whatsappText += `書包重量: ${bagWeight}kg\n`;
+  }
   if (checkedTexts.length > 0) {
-    checkedItemsText.innerText = checkedTexts.join("\n");
-    checkedItemsArea.style.display = "block";
-    const query = Array.from(checkedTexts).map(t => {return "entry.1196993608=" + t;});
-    googleFormAnchor.href = googleFormURL + "?" + query.join("&");
-    whatsappFormAnchor.href = whatsappFormURL + "%0A%0A以下是勾選的居家檢查項目：%0A" + checkedTexts.join("%0A");
-
-  } else {
-    checkedItemsText.innerText = "";
-    checkedItemsArea.style.display = "none";
-    googleFormAnchor.href = googleFormURL;
-    whatsappFormAnchor.href = whatsappFormURL;
+    const checkboxQueries = checkedTexts.map(t => "entry.1196993608=" + encodeURIComponent(t));
+    googleFormQuery.push(...checkboxQueries);
+    whatsappText += "以下是勾選的居家檢查項目：\n" + checkedTexts.join("\n") + "\n";
   }
 
-  const result = document.getElementById('checklist-result');
-  const group1Count = document.querySelectorAll('input.checklist-group1:checked').length;
-  const group2Count = document.querySelectorAll('input.checklist-group2:checked').length;
-  const group3Count = document.querySelectorAll('input.checklist-group3:checked').length;
-  const group4Count = document.querySelectorAll('input.checklist-group4:checked').length;
+  // Apply results to all links
+  googleFormAnchor.href = googleFormURL + "?" + googleFormQuery.join("&");
+  whatsappFormAnchor.href = whatsappFormURL + "?text=" + encodeURIComponent("你好，我想進行線上書包與脊椎健康初步評估。\n\n" + whatsappText);
+  whatsappConsultation = whatsappFormURL + "?text=" + encodeURIComponent("你好，我想了解有關書包與脊椎健康的首診評估。\n\n" + whatsappText);
 
-  // For displaying result
+  // Apply result to checkedItemsArea
+  if (checkedTexts.length > 0) {
+    checkedItemsArea.style.display = "block";
+    checkedItemsText.innerText = checkedTexts.join("\n");
+  } else {
+    checkedItemsArea.style.display = "none";
+    checkedItemsText.innerText = "";
+  }
+
+  // Apply result to checkboxResultArea
   let message = `
     <ol>
       <li><span style="color: green;">請繼續保持親子運動。</span></li>
     </ol>
   `;
-  if (group1Count >= 1 || group2Count >= 1 || group3Count >= 1 || group4Count >= 2) {
-    let url = "https://wa.me/85263578708?text=你好，我想了解有關書包與脊椎健康的首診評估。";
-    if (checkedTexts.length > 0) {
-      url = url + "%0A%0A以下是勾選的居家檢查項目：%0A" + checkedTexts.join("%0A");
-    }
+  if (checkboxGroup1Count >= 1 || checkboxGroup2Count >= 1 || checkboxGroup3Count >= 1 || checkboxGroup4Count >= 2) {
     message = `
       <ol>
         <li><span style="color: blue;">請繼續保持親子運動。</span></li>
-        <li><span style="color: blue;">孩子可能存在姿勢問題。</span><a href="${url}" rel="noopener" target="_blank"><span> [按此預約邱忠榮物理治療首診評估] <i class="fab fa-whatsapp whatsapp-icon"></i></span> </a></li>
+        <li><span style="color: blue;">孩子可能存在姿勢問題。</span><a href="${whatsappConsultation}" rel="noopener" target="_blank"><span> [按此預約邱忠榮物理治療首診評估] <i class="fab fa-whatsapp whatsapp-icon"></i></span> </a></li>
       </ol>
     `;
   }
-  result.innerHTML = message;
-}
+  checkboxResultArea.innerHTML = message;
 
-// Attach event listener to all checkboxes
-document.querySelectorAll('input[type="checkbox"]').forEach(box => {
-  box.addEventListener('change', updateChecklistStatus);
-});
-
-// Run once on load
-updateChecklistStatus();
-
-function updateWeightStatus() {
-  const result = document.getElementById('weight-result');
-  const kidWeight = parseFloat(document.getElementById('checklist-item10-1').value);
-  const bagWeight = parseFloat(document.getElementById('checklist-item10-2').value);
-
-  let message = ``;
+  // Apply result to weightResultArea
   if ( Number.isNaN(bagWeight) || Number.isNaN(kidWeight) ) {
     message = `
       <div style="color: green;">請輸入體重及書包重量</div>
@@ -117,16 +119,20 @@ function updateWeightStatus() {
   } else {
     const diff = (bagWeight - kidWeight * .1).toFixed(2);
     message = `
-      <div style="color: blue;">已超過10%建議負荷 ${diff}kg，可能增加脊椎受壓風險</div>
+      <div style="color: blue;">已比10%建議負荷超出 ${diff}kg，可能增加脊椎受壓風險</div>
     `;
   }
-  result.innerHTML = message;
+  weightResultArea.innerHTML = message;
 }
 
-// Attach event listener to all checkboxes
+// Attach event listener to all checkboxes and weight editboxes
 document.querySelectorAll('.checklist-group10').forEach(box => {
-  box.addEventListener('input', updateWeightStatus);
+  box.addEventListener('input', updateSchoolBagFormStatus);
+});
+
+document.querySelectorAll('input[type="checkbox"]').forEach(box => {
+  box.addEventListener('change', updateSchoolBagFormStatus);
 });
 
 // Run once on load
-updateWeightStatus();
+updateSchoolBagFormStatus();
